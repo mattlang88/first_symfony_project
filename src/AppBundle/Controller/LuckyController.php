0c824797f29499cbc39f433667f5336f68810049
Matthias Lang <mlang@BYON-0001064.local>
2017-05-08 07:08:36 +0200

    Included a service for logging and excluded the cache folder to enable consistent autocompletion for symfony containers. Followed this tutorial https://knpuniversity.com/screencast/symfony/services-useful-objects and added a logger.

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -13,13 +13,19 @@ class LuckyController extends Controller
 
     public function smallNumberAction($max)
     {
+        $log = $this->container->get('logger');
         $number = mt_rand(0, $max);
+        $log->addDebug("Upper limit for numbers is range is $max. The following number was generated: $number");
+
         return new Response("$max is smaller than 10!", 200);
 
     }
 
     public function bigNumberAction($max)
     {
+        $log = $this->container->get('logger');
+        $number = mt_rand(0, $max);
+        $log->addDebug("Upper limit for numbers is range is $max. The following number was generated: $number");
         $number = mt_rand(0, $max);
         return new Response("$max is bigger than 10!", 200);
 
