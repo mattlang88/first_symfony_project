f8a10e5873b5a08e90d466922098dafdaec7b27c
Matthias Lang <mlang@BYON-0001064.local>
2017-05-09 08:43:14 +0200

    Added an emoty Genus Controller with a single action method.

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class GenusController extends Controller
+{
+
+    public function showNameAction($name)
+    {
+
+    }
+
+}
