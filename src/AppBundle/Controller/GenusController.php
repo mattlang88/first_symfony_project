b946120a5d237b06e7fb5dbdb340ec8bfde48616
Matthias Lang <mlang@BYON-0001064.local>
2017-06-07 10:02:39 +0200

    Added a new route and controller which returns a JSON. Going along the tutorial  (https://knpuniversity.com/screencast/symfony/json-api#play) with the difference of using YAML instead of annotations.

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -40,4 +40,17 @@ class GenusController extends Controller
         return new Response($html) ;
     }
 
+    public function getNotesAction($name)
+    {
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+
+        $data = ['notes' => $notes ];
+        $json = json_encode($data);
+        return new Response($json);
+    }
+
 }
